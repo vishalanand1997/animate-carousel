64ce91db8f7c09e01db2c3ba6661f4d3815bf3f3
Vishal Anand <anandvishal888@gmail.com>
2024-06-28 01:37:41 +0530

    assignment of carousel animate

---
+++ src/Carousel/CarouselItem/index.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef } from "react";
+import CarouselStyle from "../styles/Carousel.module.css";
+
+function CarouselItem(props) {
+  const { name, bio, index } = props;
+  const imageUrl = `https://picsum.photos/40/80?random=${index}`;
+  const elementRef = useRef(null);
+
+  const checkOddEven = () => {
+    return (index + 1) % 2 === 0 ? "even" : "odd";
+  };
+  console.log(checkOddEven(), index + 1);
+  useEffect(() => {
+    const containerStyle = CarouselStyle.cardContainer;
+    const timer = setTimeout(() => {
+      if (checkOddEven() === "even") {
+        elementRef.current.className = `slide-down ${containerStyle}`;
+      } else {
+        elementRef.current.className = `slide-up ${containerStyle}`;
+      }
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <div ref={elementRef} className={`${CarouselStyle.cardContainer}`}>
+      <img
+        src={imageUrl}
+        alt="profile-image"
+        width={60}
+        height={60}
+        className={CarouselStyle.image}
+      />
+      <h6 className={CarouselStyle.name}>{name}</h6>
+      <p className={CarouselStyle.bio}>{bio}</p>
+    </div>
+  );
+}
+
+export default CarouselItem;
---
+++ src/Carousel/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import CarouselItem from "./CarouselItem";
+import CarouselStyle from "./styles/Carousel.module.css";
+
+const responsive = {
+  0: { items: 1 },
+  568: { items: 2 },
+  1024: { items: 4 },
+};
+
+const list = [
+  {
+    name: "Marvin McKinney",
+    bio: "Since switching to Swift Revel, I finally have peace of mind regarding my business finances. With their secure platform, I trust my data's protection completely.  ",
+  },
+  {
+    name: "Cody Fisher",
+    bio: " As a Freelancer, managing invoices and chasing payments was a constant headache. Now, with automated and secure payment I am getting paid on time, every time.",
+  },
+  {
+    name: "Jacob Jones",
+    bio: "As a budding startup, we sought a financial solution that was simple, secure, and scalable. Swift Revel checked all the boxes, enabling focus on growth.  ",
+  },
+  {
+    name: "Theresa Webb",
+    bio: "Swift Revel empowers me to manage my business finances with confidence and ease. Their integrated tax solutions ensure I'm always tax compliant.",
+  },
+  {
+    name: "Ronald Richards",
+    bio: "We needed a payment gateway that could handle our diverse customer base. Swift Revel offers a wide range of payment options, making it easy for our clients to pay.  ",
+  },
+];
+function Carousel() {
+  const items = list.map((item, index) => (
+    <CarouselItem {...item} index={index} />
+  ));
+
+  return (
+    <div className={CarouselStyle.container}>
+      <header>
+        <h1 className={CarouselStyle.heading}>What User says</h1>
+        <p className={CarouselStyle.subHeading}>
+          Testimonials that speak louder than words! Customer stories that light
+          up our day.
+        </p>
+      </header>
+      <main className={CarouselStyle.carouselItemContainer}>
+        <AliceCarousel
+          mouseTracking
+          items={items}
+          paddingLeft={50}
+          paddingRight={50}
+          responsive={responsive}
+          disableDotsControls
+          disableButtonsControls
+          autoPlay
+          autoPlayInterval={500}
+          animationDuration={500}
+        />
+      </main>
+    </div>
+  );
+}
+
+export default Carousel;
